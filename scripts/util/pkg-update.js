5befc7fe72563a1e2fdbf7f3a3be9debcc2ec281
dengjiayao <dnxbf321@gmail.com>
2018-05-04 16:53:39 +0800

    24h check once

--- scripts/util/pkg-update.js
+++ scripts/util/pkg-update.js
@@ -2,7 +2,7 @@
 * @Author: dengjiayao
 * @Date:   2017-12-27 13:14:12
 * @Last Modified by:   dengjiayao
-* @Last Modified time: 2018-02-08 17:45:03
+* @Last Modified time: 2018-05-04 10:24:35
 */
 const updateNotifier = require('update-notifier')
 const pkg = require('../../package.json')
@@ -10,7 +10,7 @@ const pkg = require('../../package.json')
 module.exports = () => {
   let notifier = updateNotifier({
     pkg: pkg,
-    updateCheckInterval: 2 * 1000
+    updateCheckInterval: 1000 * 60 * 60 * 24
   })
   notifier.notify()
 }
