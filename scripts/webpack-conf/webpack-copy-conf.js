afec40542609282cefaaff5206b09f094e304574
jiayao.deng <jiayao.deng@mail.dealmoon.com>
2018-08-01 16:15:15 +0800

    fix copy asset

--- scripts/webpack-conf/webpack-copy-conf.js
+++ scripts/webpack-conf/webpack-copy-conf.js
@@ -2,7 +2,7 @@
 * @Author: jiayao.deng
 * @Date:   2018-06-29 13:39:01
 * @Last Modified by:   jiayao.deng
-* @Last Modified time: 2018-06-29 14:24:42
+* @Last Modified time: 2018-08-01 16:14:13
 */
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
@@ -14,19 +14,13 @@ module.exports = env => {
           {
             // 拷贝图片、文字
             from: '!(dist)/**/*.+(png|jpg|gif|svg|woff2?|eot|ttf)',
-            to:
-              env === 'development'
-                ? '[path][name].[ext]'
-                : '[path][name].[hash:8].[ext]',
+            to: '[path][name].[ext]',
             toType: 'template'
           },
           {
             // 拷贝 asset 目录下除 .bl.js 文件
-            from: '!(dist)/asset/**/*!(.bl.)',
-            to:
-              env === 'development'
-                ? '[path][name].[ext]'
-                : '[path][name].[hash:8].[ext]',
+            from: 'asset/**/*!(.bl.)',
+            to: '[path][name].[ext]',
             toType: 'template'
           }
         ],
